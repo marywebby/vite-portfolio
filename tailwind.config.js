b87ceb94d15e48e755f7c55a869f68e2571e3515
marywebby <marywebby1999@gmail.com>
2024-10-16 16:50:19 -0500

    create tailwind file

---
+++ tailwind.config.js
@@ -0,0 +1,32 @@
+/** @type {import('tailwindcss').Config} */
+// tailwind.config.js
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'starry-sky': 'linear-gradient(to right, black, rgb(12, 12, 81) 100%)',
+      },
+      fontFamily: {
+        catamaran: ['Catamaran', 'sans-serif'],
+        noto: ['Noto Sans', 'sans-serif']
+      },
+      colors: {
+        'white': {
+          75: '#E2E8F0',
+        },
+      },
+    },
+  },
+  corePlugins: {
+    preflight: false, // Disable Tailwind's base styles if you need complete control
+  },
+  plugins: [],
+}
+
+
+
+
+
