20e18dc19b51c5c3e7ca4c988176ebdb1a8630a9
marywebby <marywebby1999@gmail.com>
2024-10-22 15:14:01 -0500

    fix font on experience  section

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -36,13 +36,13 @@ const Experience = () => {
             <li key={index} className='mb-12'>
               <div className='group relative grid pb-1 transition-all sm:grid-cols-8 sm:gap-8 lg:hover:!opacity-100 lg:group-hover/list:opacity-50'>
                 <div className='absolute -inset-x-4 -inset-y-4 z-0 hidden rounded-md transition motion-reduce:transition-none lg:-inset-x-6 lg:block lg:group-hover:bg-slate-800/50 lg:group-hover:shadow-[inset_0_1px_0_0_rgba(148,163,184,0.1)] lg:group-hover:drop-shadow-lg'></div>
-                <header className='z-10 mb-2 mt-1 list-none text-md font-semibold uppercase tracking-wide text-slate-500 sm:col-span-2' aria-label={experience.dateRange}>
+                <header className='z-10 mb-2 mt-1 list-none text-sm font-semibold uppercase tracking-wide text-slate-500 sm:col-span-2' aria-label={experience.dateRange}>
                   {experience.dateRange}
                 </header>
                 <div className='z-10 sm:col-span-6'>
                   <h3 className='font-medium leading-snug text-slate-200 mt-0'>
                     <a
-                      className='inline-flex items-baseline font-medium leading-tight text-slate-200 hover:text-teal-300 focus-visible:text-teal-300 group/link text-xl'
+                      className='inline-flex items-baseline font-medium leading-tight text-slate-200 hover:text-teal-300 focus-visible:text-teal-300 group/link text-base'
                       href={experience.link}
                       target='_blank'
                       rel='noreferrer noopener'
@@ -52,11 +52,11 @@ const Experience = () => {
                       <span>{experience.title} | {experience.organization}</span>
                     </a>
                   </h3>
-                  <p className='mt-2 text-lg leading-normal text-slate-400'>{experience.description}</p>
+                  <p className='mt-2 text-base leading-normal text-slate-400'>{experience.description}</p>
                   <ul className='mt-2 flex flex-wrap p-0 list-none' aria-label='Technologies used'>
                     {experience.technologies.map((tech, idx) => (
                       <li key={idx} className='mr-1.5 mt-2'>
-                        <div className='flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-sm font-medium leading-5 text-teal-300'>
+                        <div className='flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300'>
                           {tech}
                         </div>
                       </li>
@@ -69,8 +69,8 @@ const Experience = () => {
         </ol>
         <div className='mt-12'>
           <a
-            className='inline-flex items-baseline font-medium leading-tight hover:text-teal-300 focus-visible:text-teal-300 text-slate-200 group/link text-base underline'
-            href='/resume.pdf'
+            className='inline-flex items-baseline font-medium leading-tight hover:text-teal-300 focus-visible:text-teal-300 text-slate-200 group/link text-sm'
+            href='google.com'
             target='_blank'
             rel='noreferrer noopener'
             aria-label='View Full Résumé (opens in a new tab)'
