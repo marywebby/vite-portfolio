6ae2019930aa50c56b339e762176d58f91eba292
marywebby <marywebby1999@gmail.com>
2024-11-20 11:29:38 -0600

    changed mobile headers background color

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const About = () => {
   return (
     <section id='about' className='mb-16 scroll-mt-16 md:mb-16 lg:mb-20 lg:scroll-mt-24 text-gray-400 text-md backdrop-blur-sm p-2 rounded-md' aria-label='About me'>
-    <div className='sticky top-0 z-20 -mx-6 mb-4 w-screen bg-slate-900/75 px-6 py-5 backdrop-blur md:-mx-12 md:px-12 lg:sr-only lg:relative lg:top-auto lg:mx-auto lg:w-full lg:px-0 lg:py-0 lg:opacity-0'>
+    <div className='sticky top-0 z-20 -mx-6 mb-4 w-screen bg-slate-950/75 bg-blend-soft-light px-6 py-5 backdrop-blur md:-mx-12 md:px-12 lg:sr-only lg:relative lg:top-auto lg:mx-auto lg:w-full lg:px-0 lg:py-0 lg:opacity-0'>
       <h2 className="text-sm font-bold uppercase tracking-widest text-white-75 lg:sr-only">
         about me
       </h2>
