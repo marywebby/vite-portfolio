17295a550e428cac3f56fdf1493871c232cc2402
marywebby <marywebby1999@gmail.com>
2024-11-21 15:35:48 -0600

    shift experience over in mobile

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -51,7 +51,7 @@ const experienceData = [
 const Experience = () => {
   return (
     <section id='experience' className='mb-16 scroll-mt-16 md:mb-16 lg:mb-16 lg:scroll-mt-24 backdrop-blur-sm p-2 rounded-md' aria-label='Work Experience'>
-      <div className='sticky top-0 z-20 -mx-6 mb-4 w-screen bg-slate-950/75 px-6 py-5 backdrop-blur md:-mx-12 md:px-12 lg:sr-only lg:relative lg:top-auto lg:mx-auto lg:w-full lg:px-0 lg:py-0 lg:opacity-0'>
+      <div className='sticky top-0 z-20 -mx-10 mb-4 w-screen bg-slate-950/75 px-6 py-5 backdrop-blur md:-mx-12 md:px-12 lg:sr-only lg:relative lg:top-auto lg:mx-auto lg:w-full lg:px-0 lg:py-0 lg:opacity-0'>
         <h2 className='text-sm font-bold uppercase tracking-widest text-slate-200 lg:sr-only'>
           Experience
         </h2>
